475022fb72e6a5e3c14fcad16ce613c25dc3220f
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-04-25 14:40:00 +1000

    Made lock indicators look nicer
    Fixed volume indicator progress not changing colour on mute
    Lock indicators now horizontal

--- ags/.config/ags/scripts/lock-notifiers.sh
+++ ags/.config/ags/scripts/lock-notifiers.sh
@@ -4,14 +4,18 @@ KEYBOARD=/dev/input/by-path/platform-i8042-serio-0-event-kbd
 CAPS_EVENT="type 17 (EV_LED), code 1 (LED_CAPSL), value"
 NUM_EVENT="type 17 (EV_LED), code 0 (LED_NUML), value"
 
+agsifexists() {
+    pgrep ags && ags "$@"
+}
+
 evtest "$KEYBOARD" | while read line; do
     if [[ "$line" == *"$CAPS_EVENT 1" ]]; then
-        ags -r "isCapsLockOn.value = true;"
+        agsifexists -r "isCapsLockOn.value = true;"
     elif [[ "$line" == *"$CAPS_EVENT 0" ]]; then
-        ags -r "isCapsLockOn.value = false;"
+        agsifexists -r "isCapsLockOn.value = false;"
     elif [[ "$line" == *"$NUM_EVENT 1" ]]; then
-        ags -r "isNumLockOn.value = true;"
+        agsifexists -r "isNumLockOn.value = true;"
     elif [[ "$line" == *"$NUM_EVENT 0" ]]; then
-        ags -r "isNumLockOn.value = false;"
+        agsifexists -r "isNumLockOn.value = false;"
     fi
 done
