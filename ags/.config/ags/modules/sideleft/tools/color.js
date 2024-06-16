d1aeae5d27d0521af0391a2c0c10b1b00efe9b0b
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-06-17 09:45:46 +1000

    Updated colour picker tool
    Updated colour picker tool code + changed copy
    Updated yay config

--- ags/.config/ags/modules/sideleft/tools/color.js
+++ ags/.config/ags/modules/sideleft/tools/color.js
@@ -1,17 +1,12 @@
-// It's weird, I know
-const { Gio, GLib } = imports.gi;
-import Service from 'resource:///com/github/Aylur/ags/service.js';
-import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
-const { exec, execAsync } = Utils;
-import { clamp } from '../../.miscutils/mathfuncs.js';
+import { clamp } from "../../.miscutils/mathfuncs.js";
 
 export class ColorPickerSelection extends Service {
     static {
         Service.register(this, {
-            'picked': [],
-            'assigned': ['int'],
-            'hue': [],
-            'sl': [],
+            picked: [],
+            assigned: ["int"],
+            hue: [],
+            sl: [],
         });
     }
 
@@ -19,44 +14,49 @@ export class ColorPickerSelection extends Service {
     _xAxis = 94;
     _yAxis = 80;
 
-    get hue() { return this._hue; }
+    get hue() {
+        return this._hue;
+    }
     set hue(value) {
         this._hue = clamp(value, 0, 360);
-        this.emit('hue');
-        this.emit('picked');
-        this.emit('changed');
+        this.emit("hue");
+        this.emit("picked");
+        this.emit("changed");
     }
-    get xAxis() { return this._xAxis; }
+    get xAxis() {
+        return this._xAxis;
+    }
     set xAxis(value) {
         this._xAxis = clamp(value, 0, 100);
-        this.emit('sl');
-        this.emit('picked');
-        this.emit('changed');
+        this.emit("sl");
+        this.emit("picked");
+        this.emit("changed");
     }
-    get yAxis() { return this._yAxis; }
+    get yAxis() {
+        return this._yAxis;
+    }
     set yAxis(value) {
         this._yAxis = clamp(value, 0, 100);
-        this.emit('sl');
-        this.emit('picked');
-        this.emit('changed');
+        this.emit("sl");
+        this.emit("picked");
+        this.emit("changed");
     }
     setColorFromHex(hexString, id) {
         const hsl = hexToHSL(hexString);
         this._hue = hsl.hue;
         this._xAxis = hsl.saturation;
         // this._yAxis = hsl.lightness;
-        this._yAxis = (100 - hsl.saturation / 2) / 100 * hsl.lightness;
+        this._yAxis = ((100 - hsl.saturation / 2) / 100) * hsl.lightness;
         // console.log(this._hue, this._xAxis, this._yAxis)
-        this.emit('assigned', id);
-        this.emit('changed');
+        this.emit("assigned", id);
+        this.emit("changed");
     }
 
     constructor() {
         super();
-        this.emit('changed');
+        this.emit("changed");
     }
 }
-
 
 export function hslToRgbValues(h, s, l) {
     h /= 360;
@@ -81,11 +81,7 @@ export function hslToRgbValues(h, s, l) {
         b = hue2rgb(p, q, h - 1 / 3);
     }
     const to255 = x => Math.round(x * 255);
-    r = to255(r);
-    g = to255(g);
-    b = to255(b);
-    return `${Math.round(r)},${Math.round(g)},${Math.round(b)}`;
-    // return `rgb(${r},${g},${b})`;
+    return `${to255(r)}, ${to255(g)}, ${to255(b)}`;
 }
 export function hslToHex(h, s, l) {
     h /= 360;
@@ -167,9 +163,12 @@ export function hexToHSL(hex) {
     var g = parseInt(result[2], 16);
     var b = parseInt(result[3], 16);
 
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
+    (r /= 255), (g /= 255), (b /= 255);
+    var max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    var h,
+        s,
+        l = (max + min) / 2;
 
     if (max == min) {
         h = s = 0; // achromatic
@@ -177,9 +176,15 @@ export function hexToHSL(hex) {
         var d = max - min;
         s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
         switch (max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
         }
         h /= 6;
     }
@@ -193,6 +198,6 @@ export function hexToHSL(hex) {
     return {
         hue: h,
         saturation: s,
-        lightness: l
+        lightness: l,
     };
 }
