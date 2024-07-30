834fdf10c52be87f8c4d8328cb0b533037d7d5c3
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-07-30 13:24:14 +1000

    Create merge.sh

---
+++ merge.sh
@@ -0,0 +1,18 @@
+#!/bin/fish
+
+cd (dirname (status -f)) || exit
+
+# Merge without committing or fast forward
+git merge --no-commit --no-ff $argv[1] || exit
+
+# Revert file changes
+for pathspec in \
+        .others/pkglist.txt \
+        ags/.config/ags/scripts/templates/fuzzel/ \
+        fuzzel/.config/fuzzel/fuzzel.ini \
+        wlogout/.config/wlogout/style.css \
+        ags/.config/ags/scss/_bar.scss \
+        hypr/.config/hypr/hyprland/specific.conf
+    git reset HEAD $pathspec
+    git checkout -- $pathspec
+end
