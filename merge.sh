e4b94e8e69a9fff4aa192f259acb6fc7755a1c8c
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-07-30 13:21:49 +1000

    Track hyprland config per branch

--- merge.sh
+++ merge.sh
@@ -6,8 +6,13 @@ cd (dirname (status -f)) || exit
 git merge --no-commit --no-ff $argv[1]
 
 # Revert file changes
-set exclude .others/pkglist.txt ags/.config/ags/scripts/templates/fuzzel/ fuzzel/.config/fuzzel/fuzzel.ini wlogout/.config/wlogout/style.css ags/.config/ags/scss/_bar.scss
-for pathspec in $exclude
+for pathspec in \
+        .others/pkglist.txt \
+        ags/.config/ags/scripts/templates/fuzzel/ \
+        fuzzel/.config/fuzzel/fuzzel.ini \
+        wlogout/.config/wlogout/style.css \
+        ags/.config/ags/scss/_bar.scss \
+        hypr/.config/hypr/hyprland/specific.conf
     git reset HEAD $pathspec
     git checkout -- $pathspec
 end
