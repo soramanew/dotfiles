a280898f9db8fe0c53df1a7c06963083b18e6af2
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-02 21:33:11 +1000

    Update merge.sh to laptop branch

--- merge.sh
+++ merge.sh
@@ -8,10 +8,8 @@ git merge --no-commit --no-ff $argv[1]
 # Revert file changes
 for pathspec in \
         .others/pkglist.txt \
-        ags/.config/ags/scripts/templates/fuzzel/ \
-        fuzzel/.config/fuzzel/fuzzel.ini \
         wlogout/.config/wlogout/style.css \
-        ags/.config/ags/scss/_bar.scss \
+        ags/.config/ags/scss/_specific.scss \
         hypr/.config/hypr/hyprland/specific.conf
     git reset HEAD $pathspec
     git checkout -- $pathspec
