0343bb126dc57e916ab5b88f63d49775ec77d3e7
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-03 13:16:07 +1000

    Firefox script use firefox keybind
    Just use the firefox new window keybind to open new windows... fixes so many problems
    Updated other open scripts to disown process and hopefully more efficient jq condition?
    Communication windows also move to correct ws same as music

--- hypr/.config/hypr/scripts/firefox-new-window.sh
+++ hypr/.config/hypr/scripts/firefox-new-window.sh
@@ -1,33 +1,9 @@
 #!/bin/fish
 
-function get_firefox_addr
-    # Address of newly opened window, gotten by match workspace, class, title and sort by focus history
-    hyprctl clients -j | jq -r '[.[] | select(.workspace.name == '(hyprctl activeworkspace -j | jq '.name')' and .class == "firefox" and .title == "Mozilla Firefox")] | sort_by(.focusHistoryID)[0].address'
-end
-
-if hyprctl -j clients | jq -e '[.[]] | map(select(.class == "feishin")) == []'
+if ! hyprctl -j clients | jq -e 'first(.[] | select(.class == "firefox"))'
     # First firefox startup
     firefox & disown
 else
-    set window_addr (get_firefox_addr)
-
-    # Open new window
-    firefox
-
-    # Attempt to open sidebery tabs sidebar
-    set attempts 0
-    while [ $attempts -le 5 ]
-        sleep .1  # Some delay for the window to open
-        set new_window_addr (get_firefox_addr)
-        # Window exists and is not the same as previous before opened
-        if [ "$new_window_addr" != 'null' -a "$new_window_addr" != "$window_addr" ]
-            # Send shortcut to toggle sidebery
-            hyprctl dispatch sendshortcut "Ctrl, E, address:$new_window_addr"
-            break
-        end
-        set attempts (math $attempts + 1)
-    end
-
-    # Print if out of attempts
-    [ $attempts -le 5 ] || echo "Out of attempts: unable to toggle Sidebery for newly opened Firefox window."
+    # Use firefox shortcut to open new window so sidebar stays open
+    hyprctl dispatch sendshortcut "Ctrl, N, address:$(hyprctl clients -j | jq -r 'first(.[] | select(.class == "firefox")).address')"
 end
