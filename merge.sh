c9767fef9385d0dc1ade03718702ff7a2fee13c9
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-03 13:31:23 +1000

    Merge script more verbose

--- merge.sh
+++ merge.sh
@@ -2,15 +2,26 @@
 
 cd (dirname (status -f)) || exit
 
+function colour_echo -a text
+    set_color --bold cyan
+    echo "$text"
+    set_color normal
+end
+
 if [ (git status --porcelain) ]
-    read -l -P ':: You have unstashed changes. Proceed with merge? [y/N] ' confirm
+    read -l -p "colour_echo ':: You have unstashed changes. Proceed with merge? [y/N] '" confirm
     if [ "$confirm" != 'y' -a "$confirm" != 'Y' ]
+        colour_echo 'Exiting.'
         exit
     end
 end
 
+colour_echo 'Starting merge...'
+
 # Merge without committing or fast forward
 git merge --no-commit --no-ff $argv[1]
+
+colour_echo 'Reverting files...'
 
 # Revert file changes
 for pathspec in \
@@ -21,3 +32,5 @@ for pathspec in \
     git reset HEAD $pathspec
     git checkout -- $pathspec
 end
+
+colour_echo 'Finished merge.'
