7471a499072588222fecc15953e96756bb5ac6d0
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-07 12:37:29 +1000

    Fix init wall + more efficient change wall
    Install and autostart snixembed for safeeyes tray icon
    Change wallpaper script:
     - better help message
     - fix no filter option
     - more efficient identify

--- ags/.config/ags/scripts/color_generation/init-wallpaper.sh
+++ ags/.config/ags/scripts/color_generation/init-wallpaper.sh
@@ -3,5 +3,5 @@
 if [ "$(cat ~/.cache/ags/user/wallpaper/enabled.txt)" = 'true' ]
     ~/.config/ags/scripts/color_generation/change-wallpaper.sh
 else
-    ~/.config/ags/scripts/color_generation/change-wallpaper.sh "$(cat ~/.cache/ags/user/wallpaper/last.txt)"
+    ~/.config/ags/scripts/color_generation/change-wallpaper.sh -f "$(cat ~/.cache/ags/user/wallpaper/last.txt)"
 end
