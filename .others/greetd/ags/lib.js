2699eb9c157c1ed0d0f3916bf6ef92bafea3562d
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-09 23:03:33 +1000

    greeter: better clock visibility + random bgs
    Text shadow for better time and date visibility
    Random background, same as change-wallpaper.sh

--- .others/greetd/ags/lib.js
+++ .others/greetd/ags/lib.js
@@ -1,4 +1,5 @@
 import Gdk from "gi://Gdk";
+const Hyprland = await Service.import("hyprland");
 
 const display = Gdk.Display.get_default();
 const defaultCursor = Gdk.Cursor.new_from_name(display, "default");
@@ -11,3 +12,25 @@ export const setupCursorHover = button => {
 };
 
 export const CACHE_DIR = "/var/cache/greeter";
+
+const chooseBackground = () => {
+    const lastBackground = Utils.readFile(`${CACHE_DIR}/last-background.txt`);
+    const { width, height } = JSON.parse(Hyprland.message("j/monitors"))[0];
+    const backgrounds = Utils.exec(`find ${App.configDir}/backgrounds/ -type f -not -wholename '${lastBackground}'`)
+        .split("\n")
+        // Filter by size
+        .filter(b => {
+            const [w, h] = Utils.exec(`identify -ping -format '%w %h' ${b}`).split(" ");
+            return w > width * 0.8 && h > height * 0.8;
+        });
+
+    // No other backgrounds in folder
+    if (!backgrounds.length) return lastBackground;
+
+    // Get random
+    const background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+    Utils.writeFile(background, `${CACHE_DIR}/last-background.txt`);
+    return background;
+};
+
+export const background = chooseBackground();
