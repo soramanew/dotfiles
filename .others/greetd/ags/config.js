669edb4a0d77e608cc3478e08b861ddec703e1e1
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-09 23:03:33 +1000

    greeter: better clock visibility + random bgs
    Text shadow for better time and date visibility
    Random background, same as change-wallpaper.sh

--- .others/greetd/ags/config.js
+++ .others/greetd/ags/config.js
@@ -1,5 +1,5 @@
 const { Window, Box, Overlay } = Widget;
-import { CACHE_DIR } from "./lib.js";
+import { background, CACHE_DIR } from "./lib.js";
 import TimeDate from "./modules/timedate.js";
 import Session from "./modules/session.js";
 import Auth from "./modules/auth.js";
@@ -13,14 +13,14 @@ const win = Window({
         child: Box({
             expand: true,
             className: "background",
-            css: `background-image: url("${App.configDir}/assets/background");`,
+            css: `background-image: url("${background}");`,
         }),
         overlays: [TimeDate(), Session(), Auth()],
     }),
 });
 
 Utils.exec(
-    `bash -c '${App.configDir}/scripts/generate_colors_material.py --path "${App.configDir}/assets/background" > ${CACHE_DIR}/_material.scss'`
+    `bash -c '${App.configDir}/scripts/generate_colors_material.py --path "${background}" > ${CACHE_DIR}/_material.scss'`
 );
 Utils.exec(`sass -I ${CACHE_DIR} ${App.configDir}/scss/main.scss ${CACHE_DIR}/style.css`);
 App.config({ style: `${CACHE_DIR}/style.css`, cursorTheme: "sweet-cursors", windows: [win] });
--- .scripts/install-greeter.sh
+++ .scripts/install-greeter.sh
@@ -1,20 +1,24 @@
 #!/bin/fish
 
 cd (dirname (status filename)) || exit
-cd ..  # Move to project root
+cd .. || exit  # Move to project root
+
+# User to set as owner (for easy debugging)
+[ -n "$argv[1]" ] && set perm $argv[1] || set perm greeter
 
 # Reset config
 [ -d '/etc/greetd' ] && sudo rm -r /etc/greetd
 sudo cp -r .others/greetd /etc/greetd
 
 # Create cache dir tmpfile config
-echo 'd /var/cache/greeter - greeter greeter - -' | sudo tee /etc/tmpfiles.d/greeter.conf
-echo 'd /var/cache/greeter/faces - greeter greeter - -' | sudo tee -a /etc/tmpfiles.d/greeter.conf
+echo "d /var/cache/greeter - $perm $perm - -" | sudo tee /etc/tmpfiles.d/greeter.conf
+echo "d /var/cache/greeter/faces - $perm $perm - -" | sudo tee -a /etc/tmpfiles.d/greeter.conf
+echo "d /var/cache/greeter/backgrounds - $perm $perm - -" | sudo tee -a /etc/tmpfiles.d/greeter.conf
 
 # Add user faces tmpfile config
 for user in (find /home -maxdepth 1 -mindepth 1 -type d -not -name 'lost+found' -exec basename {} \;)
     if [ -f "/home/$user/.face" ]
-        echo "C+ /var/cache/greeter/faces/$user - greeter greeter - $(realpath /home/$user/.face)" | sudo tee -a /etc/tmpfiles.d/greeter.conf
+        echo "C+ /var/cache/greeter/faces/$user - $perm $perm - $(realpath /home/$user/.face)" | sudo tee -a /etc/tmpfiles.d/greeter.conf
     end
 end
 
