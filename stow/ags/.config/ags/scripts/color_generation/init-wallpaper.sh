28ffbd0764afc85c51a017ffc6df0daded9cc382
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-10 20:47:52 +1000

    scripts: moved wallpaper scripts
    Moved from ags/scripts to accessible by dotctl
    hypr: Don't open todoscreen on init
    applycolor.sh: fix cp error if directory doesn't exist

--- stow/ags/.config/ags/scripts/color_generation/init-wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/fish
-
-if [ "$(cat ~/.cache/ags/user/wallpaper/enabled.txt)" = 'true' ]
-    ~/.config/ags/scripts/color_generation/change-wallpaper.sh
-else
-    ~/.config/ags/scripts/color_generation/change-wallpaper.sh -f "$(cat ~/.cache/ags/user/wallpaper/last.txt)"
-end
