89d77c65e2ce868b5df316950a171f56011531c1
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-10 20:32:44 +1000

    firefox: update shyfox install script
    Loop through all folders for the given profile
    
    All scripts:
     - no bold output

--- scripts/install/shyfox.sh
+++ scripts/install/shyfox.sh
@@ -9,24 +9,29 @@ cd ../.. || exit  # Move to project root
 # Source utils file
 . scripts/_util.sh
 
-set profile_path (find ~/.mozilla/firefox -type d -name '*.'$profile)[1]
-if [ -z "$profile_path" ]
+set profile_paths (find ~/.mozilla/firefox -type d -name '*.'$profile)
+if ! count $profile_paths >/dev/null
     output "Unable to get path for $profile. Exiting."
     exit 1
 end
 
-if [ -e "$profile_path/chrome" ]
-    output "Existing $profile_path/chrome found. Deleting..."
-    rm -r $profile_path/chrome
-end
+for profile_path in $profile_paths
+    set -l pp_stripped (basename $profile_path)
+    output "Installing ShyFox to $pp_stripped..."
 
-if [ -e "$profile_path/user.js" ]
-    output "Existing $profile_path/user.js found. Deleting..."
-    rm $profile_path/user.js
-end
+    if [ -e "$profile_path/chrome" ]
+        output "Existing $pp_stripped/chrome found. Deleting..."
+        rm -r $profile_path/chrome
+    end
 
-output "Symlinking ShyFox chrome/ and user.js to $profile_path..."
-ln -s (realpath firefox/ShyFox/chrome) $profile_path/chrome
-ln -s (realpath firefox/ShyFox/user.js) $profile_path/user.js
+    if [ -e "$profile_path/user.js" ]
+        output "Existing $pp_stripped/user.js found. Deleting..."
+        rm $profile_path/user.js
+    end
+
+    output "Symlinking ShyFox chrome/ and user.js to $pp_stripped..."
+    ln -s (realpath firefox/ShyFox/chrome) $profile_path/chrome
+    ln -s (realpath firefox/ShyFox/user.js) $profile_path/user.js
+end
 
 output 'Done!'
