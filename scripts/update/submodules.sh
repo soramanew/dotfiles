20300c550911541f2804b7e43849640bb7157a3b
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-08-10 18:33:43 +1000

    submodules: add init flag to update script

--- scripts/update/submodules.sh
+++ scripts/update/submodules.sh
@@ -3,4 +3,4 @@
 cd (dirname (status filename)) || exit
 cd ../.. || exit  # Move to project root
 
-git submodule update --recursive --remote $argv
+git submodule update --init --recursive --remote $argv
