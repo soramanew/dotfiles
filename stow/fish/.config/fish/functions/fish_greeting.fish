7da2286c79418af90fe27bf1f42bb9dda2076d21
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-09-12 19:21:02 +1000

    Fix fish greeting (cowfortune broken)
    Cowfortune is broken or something so just do it manually

--- stow/fish/.config/fish/functions/fish_greeting.fish
+++ stow/fish/.config/fish/functions/fish_greeting.fish
@@ -1,6 +1,6 @@
 function fish_greeting
-sleep .1
-hostnamectl hostname | figlet -tcf chiseled
-echo
-cowfortune | figlet -tcf term
+    sleep .1
+    hostnamectl hostname | figlet -tcf chiseled
+    echo
+    fortune | cowsay -r | figlet -tcf term
 end
