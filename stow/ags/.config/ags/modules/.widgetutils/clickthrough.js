e9facb24570e1fb3d02f7f78e048c1e4aacf8f53
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-09-17 10:58:48 +1000

    clickthrough: window toggle -> size alloc
    Use size allocate signal instead of window toggle cause that is what invalidates it

--- stow/ags/.config/ags/modules/.widgetutils/clickthrough.js
+++ stow/ags/.config/ags/modules/.widgetutils/clickthrough.js
@@ -1,14 +1,5 @@
 import Cairo from "cairo";
 
 export const dummyRegion = new Cairo.Region();
-const setRegion = self => self.window.input_shape_combine_region(dummyRegion, 0, 0);
-export const enableClickthrough = self => {
-    self.hook(
-        App,
-        (_, name, visible) => {
-            if (visible && name == self.name) setRegion(self);
-        },
-        "window-toggled"
-    );
-    Utils.timeout(1, () => setRegion(self));
-};
+export const enableClickthrough = self =>
+    Utils.timeout(1, () => self.on("size-allocate", () => self.window.input_shape_combine_region(dummyRegion, 0, 0)));
