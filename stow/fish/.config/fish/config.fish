644292c6fa57d9f43143484d69b5a7a22f8c4616
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-09-29 22:29:15 +1000

    fish: remove unnecessary stuff + enable foot stuff
    Enable foot prompt jumping
    Enable ctrl+shift+n new foot window opening in same cwd (by leaving TERM env as default)

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,24 +1,14 @@
-function fish_prompt -d "Write out the prompt"
-    # This shows up as USER@HOST /home/user/ >, with the directory colored
-    # $USER and $hostname are set by fish, so you can just use them
-    # instead of using `whoami` and `hostname`
-    printf '%s@%s %s%s%s > ' $USER $hostname \
-        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-end
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting
-
+    # Starship custom prompt
     starship init fish | source
+
+    # Terminal colours
     if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
         cat ~/.cache/ags/user/generated/terminal/sequences.txt
     end
+
+    # For jumping between prompts in foot terminal
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
+    end
 end
-
-alias pamcan=pacman
-
-# function fish_prompt
-#   set_color cyan; echo (pwd)
-#   set_color green; echo '> '
-# end
